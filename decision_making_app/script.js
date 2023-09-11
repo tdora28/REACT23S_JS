885dbb0a23d71b7d00404a8c8a2cee421d54da87
Dóra Tokai <tokaidora88@gmail.com>
2023-09-11 10:06:34 +0300

    Decision Making App: Updated files for submitting

--- decision_making_app/script.js
+++ decision_making_app/script.js
@@ -1,78 +1,131 @@
-// HTML elements
+// Collect HTML buttons
+const sendBtn = document.querySelector("#sendBtn");
+const refreshBtn = document.querySelector("#refreshBtn");
 
-const submitBtn = document.querySelector('button[type="submit"]');
-const nameInput = document.querySelector("#userName");
-const questionInput = document.querySelector("#userQuestion");
-const form = document.querySelector(".form");
-const answerDiv = document.querySelector(".answer");
-const refreshBtn = document.querySelector(".button--hidden");
-
-// VARIABLES
-
-// Store values from inputs
-let userName = "";
-let userQuestion = "";
-
-// Generate random num between 1 and 10 (included)
-const randomNum = Math.floor(Math.random() * 10 + 1);
-
-// Store possible answers
-const decisionStatements = {
-  1: "The stars say yes, go for it!",
-  2: "Trust your instincts and take the leap.",
-  3: "Hmm, better try again later.",
-  4: "Consider all options before making a move.",
-  5: "Outlook is bright, proceed with confidence.",
-  6: "Seek advice from a trusted friend before deciding.",
-  7: "Signs point to unexpected opportunities.",
-  8: "It's a toss-up, make a choice and see what happens.",
-  9: "Take a step back and reassess before moving forward.",
-  10: "Not the right time, patience will bring better results.",
-};
-
-// FUNCTIONS
-
-// Return (string) greeting depending on user input
-const generateGreeting = (userName, userQuestion) => {
-  let greeting = `
-    ${userName === "" ? "Hello!" : `Hello, ${userName}!`}
-    Your question was: "${userQuestion}" &ndash;
-    And the answer is:
-  `;
-
-  return greeting;
-};
-
-// Render UI that shows the answer
-// 1. hide form
-// 2. display answer div with appropriate greeting and randomly picked answer
-// 3. display refresh button
-const renderAnswerUI = (name, question) => {
-  form.style.display = "none";
-
-  answerDiv.style.display = "flex";
-  answerDiv.innerHTML = `
-  ${generateGreeting(name, question)}
-  <span>${decisionStatements[randomNum]}</span>
-`;
-
+// Hide .questions screen and #sendBtn
+// Reveal .decision screen and #refreshBtn
+const switchMachineScreen = () => {
+  document.querySelector(".questions").style.display = "none";
+  sendBtn.style.display = "none";
+  document.querySelector(".decision").style.display = "block";
   refreshBtn.style.display = "block";
 };
 
-// EVENT LISTENERS
+// Write error message inside .decision__answer
+const buildErrorMsg = () => {
+  document.querySelector(".decision__answer").textContent =
+    "Please enter a question!";
+};
 
-// Form submit button
-// On click: 1.) catch input values, 2.) render UI to show the greeting and the answer
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
+// Build greeting text for .decision__greeting
+// If nothing is given or the string is only white space: "Hello!"
+// Otherwise include the given string (text) in the greeting
+const buildGreeting = (text) => {
+  document.querySelector(".decision__greeting").textContent =
+    !text || !text.replace(/\s/g, "").length ? "Hello!" : `Hello, ${text}!`;
+};
 
-  userName = nameInput.value;
-  userQuestion = questionInput.value;
+// Build a short summary text for .decision__summary
+// Include the given string (text)
+const buildSummary = (text) => {
+  document.querySelector(
+    ".decision__summary"
+  ).textContent = `The answer to "${text}" is:`;
+};
 
-  renderAnswerUI(userName, userQuestion);
-});
+// Pick an answer based on the generated random number (1-10)
+const pickAnswer = (num) => {
+  let text = "";
 
-// Refresh button
+  switch (num) {
+    case 1:
+      text = "Signs point to yes, like stars in a galaxy far, far away.";
+      break;
+    case 2:
+      text = "As I see it, yes, a brighter tomorrow awaits.";
+      break;
+    case 3:
+      text = "It is decidedly so, destiny aligns.";
+      break;
+    case 4:
+      text = "Outlook good, the planets align in your favor.";
+      break;
+    case 5:
+      text = "Outlook not so stellar, recalibrate and try again.";
+      break;
+    case 6:
+      text = "Don't count on it, the cosmic winds are not in your favor";
+      break;
+    case 7:
+      text = "My cosmic circuits say no, tread carefully.";
+      break;
+    case 8:
+      text = "Better not tell you now, the fates are still weaving.";
+      break;
+    case 9:
+      text = "Ask again later, the interstellar signals are unclear.";
+      break;
+    case 10:
+      text = "Concentrate and ask again, your aura is too fuzzy.";
+      break;
+    default:
+      text = "No idea!";
+      break;
+  }
+
+  document.querySelector(".decision__answer").textContent = text;
+};
+
+// Check user input
+// Populate UI with correct messages
+const decisionMachine = () => {
+  const userName = document.querySelector("#userName").value;
+  const question = document.querySelector("#userQuestion").value;
+  const randomNum = Math.floor(Math.random() * 10 + 1);
+
+  switchMachineScreen();
+
+  if (!question || !question.replace(/\s/g, "").length) {
+    buildErrorMsg();
+  } else {
+    setFace(randomNum);
+    buildGreeting(userName);
+    buildSummary(question);
+    pickAnswer(randomNum);
+  }
+};
+
+// Add event listener to submit btn
+sendBtn.addEventListener("click", decisionMachine);
 refreshBtn.addEventListener("click", () => {
-  window.location.reload();
+  location.reload();
 });
+
+// Display the correct child of .face__mouth div depending on the given number (num) by toggling the .show-face class
+// The numbers correspond to the answers, which all have an emotion tied to them (happy, sad, neutral)
+const setFace = (num) => {
+  let faces = document.querySelectorAll(".face__mouth>*");
+  let search = "";
+
+  if (num < 5) {
+    search = "face__happy";
+  } else if (num < 8) {
+    search = "face__sad";
+  } else {
+    search = "face__neutral";
+  }
+
+  for (let face of faces) {
+    if (
+      face.classList.contains("show-face") &&
+      !face.classList.contains(search)
+    ) {
+      face.classList.remove("show-face");
+    } else if (
+      !face.classList.contains("show-face") &&
+      face.classList.contains(search)
+    ) {
+      face.classList.add("show-face");
+    }
+  }
+};
