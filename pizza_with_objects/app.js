0b6d33bf029356c1dfd345bd6f5988e0b76fe393
tdora28 <tokaidora88@gmail.com>
2023-09-18 19:25:49 +0300

    Pizza with Objects: Add basic HTML structure

--- pizza_with_objects/app.js
+++ pizza_with_objects/app.js
@@ -6,16 +6,27 @@ Task 2
 Write code which models an order to a pizza place as a class. The order has a customer name, delivery type, and there can be several pizzas in one order. Order can be updated by adding pizzas to it with a method of the order class.
 */
 
-class Pizza {
-  name;
-  toppings = [];
-  basePrice = 0; // In cents! Don't use floating point nums for money!
-  size = 2;
+// class Pizza {
+//   name;
+//   toppings = [];
+//   basePrice = 0; // In cents! Don't use floating point nums for money!
+//   size = 2;
 
-  getPrice() {
-    return this.basePrice;
+//   getPrice() {
+//     return this.basePrice;
+//   }
+// }
+
+// const americano = new Pizza();
+// americano.name = 'americano';
+
+const pizzaShowcase = document.querySelector('#pizzaShowcase');
+
+class Pizza {
+  constructor(name, toppings, basePrice, size) {
+    this.name = name;
+    this.toppings = toppings;
+    this.basePrice = basePrice;
+    this.size = size;
   }
 }
-
-const americano = new Pizza();
-americano.name = 'americano';
