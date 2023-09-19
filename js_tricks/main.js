01fb10c26652a7909ef6304d681c777b8cc44846
Dóra Tokai <tokaidora88@gmail.com>
2023-09-19 13:17:08 +0300

    JS Tricks: Add header bg on scroll and set scroll-margin-top

--- js_tricks/main.js
+++ js_tricks/main.js
@@ -1,17 +1,27 @@
+const header = document.querySelector("header");
 const backButton = document.querySelector("#backToTop");
 const mobButton = document.querySelector(".mobile");
 const nav = document.querySelector("nav ul");
+const menuItems = document.querySelectorAll("nav ul li a");
 
 window.onscroll = function () {
   scrollFunction();
 };
 
-// Code from W3S for hiding btn on scroll
+// Code from W3S
 function scrollFunction() {
+  // Hiding back to top btn on scroll
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     backButton.style.display = "block";
   } else {
     backButton.style.display = "none";
+  }
+
+  // Show header bg on scroll
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("background");
+  } else {
+    header.classList.remove("background");
   }
 }
 
@@ -23,6 +33,11 @@ const getToTop = () => {
 
 // Mobile menu
 const mobMenu = () => {
+  // When link clicked, close menu
+  for (const item of menuItems) {
+    item.addEventListener("click", mobMenu);
+  }
+
   if (nav.classList.contains("responsive")) {
     nav.classList.remove("responsive");
   } else {
