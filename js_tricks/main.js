92413e02c913ddac299ecaf6883063e3b21790ef
Dóra Tokai <tokaidora88@gmail.com>
2023-09-19 12:56:07 +0300

    JS Tricks: Add mobile menu

--- js_tricks/main.js
+++ js_tricks/main.js
@@ -1,9 +1,12 @@
 const backButton = document.querySelector("#backToTop");
+const mobButton = document.querySelector(".mobile");
+const nav = document.querySelector("nav ul");
 
 window.onscroll = function () {
   scrollFunction();
 };
 
+// Code from W3S for hiding btn on scroll
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     backButton.style.display = "block";
@@ -12,9 +15,20 @@ function scrollFunction() {
   }
 }
 
+// Scroll to top
 const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 };
 
+// Mobile menu
+const mobMenu = () => {
+  if (nav.classList.contains("responsive")) {
+    nav.classList.remove("responsive");
+  } else {
+    nav.classList.add("responsive");
+  }
+};
+
 backButton.addEventListener("click", getToTop);
+mobButton.addEventListener("click", mobMenu);
