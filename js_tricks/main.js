cc5cd37144f8e5aa083b8231cee007154650bba4
Dóra Tokai <tokaidora88@gmail.com>
2023-09-19 12:04:24 +0300

    JS Tricks: Add hide effect on back to top button

--- js_tricks/main.js
+++ js_tricks/main.js
@@ -1,5 +1,17 @@
 const backButton = document.querySelector("#backToTop");
 
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backButton.style.display = "block";
+  } else {
+    backButton.style.display = "none";
+  }
+}
+
 const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
